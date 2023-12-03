d33d76c45c27eda0264a6fe442367889bef3ded1
gonzalosanmonte@gmail.com <gonzalosanmonte@gmail.com>
2023-12-04 00:37:00 +0100

    VA 64 65 VARIACIONES

---
+++ 64 variaciones con repetión/64.cpp
@@ -0,0 +1,41 @@
+﻿//DG29
+//Gonzalo S�nchez Montesinos
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+std::ostream& operator << (std::ostream& op, const std::vector<char>& v) {
+	for (auto& x : v) op << x;
+	return op;
+}
+
+void va(int m, int n, int k, std::vector<char>& sol) {
+	for (int i = 'a'; i < 'a' + m; ++i) {
+		sol[k] = i;
+		
+		if (n - 1 == k)	std::cout << sol << '\n';
+		else va(m, n, k + 1, sol);
+		
+	}
+}
+
+
+bool resuelveCasos() {
+	int m, n;
+	std::cin >> m;
+	if (!cin) return false;
+	cin >> n;
+	std::vector<char> sol(n);
+	va(m, n, 0, sol);
+	return true;
+}
+
+
+
+int main() {
+	while (resuelveCasos());
+	return 0;
+}
